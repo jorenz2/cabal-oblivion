c46b66968db3b4304977be424d6749416fff40c0
Emil Cruz <cruz.emiljorenz.c.2021@gmail.com>
2024-10-27 02:29:41 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleDownloadClick = () => {
+    window.open('https://drive.google.com/file/d/1MZsTzSJ1Tnrwp3r1dSgPpeDy-iMQmU8H/view?usp=drive_link', '_blank'); 
+  };
+
+  const handleRegisterClick = () => {
+    window.open('https://discord.gg/FA7dRWaJjY', '_blank');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Cabal Oblivion</h1>
+      <button className="download-button" onClick={handleDownloadClick}>
+        Download
+      </button>
+      <br></br>
+      <button className="download-button2" onClick={handleRegisterClick}>
+        Register
+      </button>
     </div>
   );
 }
